1511cd3c3787755d3c422ddbab96aa654373ed13
Eskilon <Julio.Jesus@globalweb.com.br>
2021-11-16 15:36:06 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.BootCamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -16,13 +18,34 @@ public class Main {
         curso2.setDescricao("Descrição curso Python");
         curso2.setCargaHoraria(7);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+        /*System.out.println(curso1);
+        System.out.println(curso2);*/
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição curso Java");
         mentoria.setData(LocalDate.now());
-        System.out.println(mentoria);
+        //System.out.println(mentoria);
+
+        BootCamp bootcamp = new BootCamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos de João: " + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos de João: " + devJoao.getConteudosConcluidos());
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos da Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos da Camila: " + devCamila.getConteudosConcluidos());
+
     }
 }
